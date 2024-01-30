9d0dbad4b9c8a5081eea16424a6f8cc828d56dae
Aditya <abalyan@una.edu>
2024-01-30 16:43:07 -0600

    updates

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5500;
 
 app.get('/', function (req, res) {
   res.send('An Enneagram 6 is called The Loyalist. They are fun, energetic, dependable, and honest at their best. In times of disintegration, they can become anxious and self-doubting, indecisive, and overly dependent on fail safes rather than their instincts or judgment.')
 })
 
-app.listen(3000)
+
+//app.listen(3000)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
